8f10320843eac31a6f6e74567e415eb1b8a68b15
ulyhwy <ulyhwy@gmial.com>
2019-03-01 18:32:37 -0600

    changed to table layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {MatExpansionModule} from '@angular/material/expansion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, MatExpansionModule, BrowserAnimationsModule,
-    MatRadioModule, MatListModule
+    BrowserModule, MatTableModule, BrowserAnimationsModule,
+    MatRadioModule, MatListModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
